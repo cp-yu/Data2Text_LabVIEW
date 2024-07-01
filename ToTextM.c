a2585c057ef1cc5d2ad08e87542e83a2ae800df9
yunxin <2232727530@qq.com>
2024-07-02 00:00:44 +0800

    fit labview

--- ToTextM.c
+++ ToTextM.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-__declspec(dllexport) void ToText(double** lists, int* lengths, int num_columns, const char* title, const char* extra_info, const char* col_names) {
+__declspec(dllexport) void ToText(double** lists, int length, int num_columns, const char* title, const char* extra_info, const char* col_names) {
     FILE *file = fopen(title, "w");
     if (file) {
         fprintf(file, "%s\n", extra_info);
 
         // Write column names
-                // Write column names
         char* col_names_copy = strdup(col_names);
         char* token = strtok(col_names_copy, ",");
         for (int i = 0; i < num_columns; i++) {
@@ -21,17 +20,8 @@ __declspec(dllexport) void ToText(double** lists, int* lengths, int num_columns,
         }
         free(col_names_copy);
 
-
-        // Find the minimum length among all columns
-        int min_len = lengths[0];
-        for (int i = 1; i < num_columns; i++) {
-            if (lengths[i] < min_len) {
-                min_len = lengths[i];
-            }
-        }
-
         // Write data rows
-        for (int i = 0; i < min_len; i++) {
+        for (int i = 0; i < length; i++) {
             for (int j = 0; j < num_columns; j++) {
                 fprintf(file, "%f%s", lists[j][i], (j == num_columns - 1) ? "\n" : "\t");
             }
--- ToTextM.py
+++ ToTextM.py
@@ -12,11 +12,11 @@ totext_dll = ctypes.CDLL(dll_path)
 # Define the ToText function argument types
 totext_dll.ToText.argtypes = [
     ctypes.POINTER(ctypes.POINTER(ctypes.c_double)),  # double** lists
-    ctypes.POINTER(ctypes.c_int),  # int* lengths
+    ctypes.c_int,  # int length
     ctypes.c_int,  # int num_columns
     ctypes.c_char_p,  # const char* title
     ctypes.c_char_p,  # const char* extra_info
-    ctypes.POINTER(ctypes.c_char_p)  # const char** col_names
+    ctypes.c_char_p  # const char* col_names
 ]
 
 # Example data
@@ -24,22 +24,20 @@ list_a = np.array([1.0, 2.0, 3.0], dtype=np.float64)
 list_b = np.array([4.0, 5.0, 6.0], dtype=np.float64)
 list_c = np.array([7.0, 8.0, 9.0], dtype=np.float64)
 lists = [list_a, list_b, list_c]
-lengths = np.array([len(list_a), len(list_b), len(list_c)], dtype=np.int32)
+length = len(list_a)
 col_names = b"Column A,Column B,Column C"
 
 # Convert lists to ctypes format
 list_pointers = (ctypes.POINTER(ctypes.c_double) * len(lists))(*[lst.ctypes.data_as(ctypes.POINTER(ctypes.c_double)) for lst in lists])
-lengths_pointers = lengths.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
-col_names_pointers = (ctypes.c_char_p * len(col_names))(*col_names)
 
 # Call the ToText function
 totext_dll.ToText(
     list_pointers,
-    lengths_pointers,
+    length,
     len(lists),
     b"output.txt",  # title
     b"This is some extra info",  # extra_info
-    col_names_pointers
+    col_names
 )
 
 print("Data has been written to output.txt")
