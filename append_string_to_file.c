209318875b499e0ce992a48640fab9f1023d2c56
yunxin <2232727530@qq.com>
2024-07-12 13:58:12 +0800

    fix bug#1

--- append_string_to_file.c
+++ append_string_to_file.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // 导出函数
 __declspec(dllexport) int append_string_to_file(const char* str, const char* file_name) {
+
+    // 获取字符串长度
+    size_t len = strlen(str);
+
+    // 检查字符串长度是否至少为3，以便修改后缀
+    if (len >= 3) {
+        // 修改最后三个字符为"txt"
+        strcpy(str + len - 3, "txt");
+    } else {
+        return -3; // 字符串长度不足
+    }
+
     // 打开文件以追加模式写入
     FILE *file = fopen(file_name, "a");
     if (file == NULL) {
         return -1; // 文件打开失败
     }
-
     // 写入新字符串和换行符
-    fprintf(file, "\n%s", str);
+    fprintf(file, "\n%s", modified_str);
 
-    // 关闭文件
+    // 释放内存并关闭文件
     fclose(file);
 
     return 0; // 成功
